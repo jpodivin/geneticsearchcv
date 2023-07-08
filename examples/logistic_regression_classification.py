f6332b167f0988feee291eb66097974f4709989d
Jiri Podivin <jpodivin@gmail.com>
2023-07-08 16:24:56 +0200

    Finalizing examples
    Signed-off-by: Jiri Podivin <jpodivin@gmail.com>

--- examples/logistic_regression_classification.py
+++ examples/logistic_regression_classification.py
@@ -2,15 +2,22 @@ import numpy as np
 from scipy.stats import uniform
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
+from geneticsearchcv import GeneticSearchCV
 
-from geneticsearchcv._search import GeneticSearchCV
+RANDOM_STATE = 1
 
 param_dists = {
     'C': uniform(loc=0, scale=4),
     'penalty': ['l2', 'l1']
 }
 
-X,y = make_classification(random_state=10)
-gc = GeneticSearchCV(LogisticRegression(solver='liblinear'), param_dists, scoring='accuracy')
+X,y = make_classification(random_state=RANDOM_STATE)
+
+gc = GeneticSearchCV(
+    LogisticRegression(solver='liblinear'),
+    param_dists,
+    scoring='accuracy',
+    random_state=RANDOM_STATE)
+
 res = gc.fit(X, y)
 print(gc.best_params_, gc.best_score_, np.mean(gc.cv_results_['mean_score_time']))
--- examples/regression_tree.py
+++ examples/regression_tree.py
@@ -3,7 +3,9 @@ from scipy.stats import uniform
 from sklearn.datasets import make_regression
 from sklearn.tree import DecisionTreeRegressor
 
-from geneticsearchcv._search import GeneticSearchCV
+from geneticsearchcv import GeneticSearchCV
+
+RANDOM_STATE = 1
 
 param_dists = {
     'max_depth': [i for i in range(1, 50)],
@@ -11,12 +13,14 @@ param_dists = {
     'criterion': ['squared_error', 'friedman_mse', 'absolute_error'],
     'ccp_alpha': uniform(0, 1)
 }
-X,y = make_regression(random_state=10)
+X,y = make_regression(random_state=RANDOM_STATE)
 gc = GeneticSearchCV(
     DecisionTreeRegressor(),
     param_dists,
-    verbose=3,
     scoring='neg_mean_squared_error',
-    n_iter=50)
+    n_iter=10,
+    random_state=RANDOM_STATE,
+    pop_size=100)
+
 res = gc.fit(X, y)
 print(gc.best_params_, gc.best_score_, np.mean(gc.cv_results_['mean_score_time']))
